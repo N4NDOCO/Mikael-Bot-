372df893701e84833b79017f09f135f99888560b
N4NDOCO <andersonsilva978x@gmail.com>
2026-01-30 12:42:21 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
+
 GUILD_ID = 1465477542919016625
+
+# Canal onde o /call é permitido
+CANAL_CALL_ID = 1465657430292697151  # ✅┃escolha-e-pagamentos
+
+# Categoria onde os tickets serão criados
+CATEGORIA_TICKET_ID = 1466819574145220732  # 📦┃Atendimentos
+
+# Cargo dos entregadores
 CARGO_ENTREGADOR = "Entregador"
-CALL_CHANNEL_ID = 1465657430292697151
